51b4f0aa9e1c75ac8b2e039b19a68e83ce51a1dc
mark blaise <markkiguli10@gmail.com>
2023-08-13 21:37:25 +0300

    alx interview prep

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print('min # of operations for {} is {}'.format(n, minOperations(n)))
+
+n = 12
+print('min # of operations for {} is {}'.format(n, minOperations(n)))
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,28 +4,26 @@ UTF-8 Validation
 """
 
 
-def validUTF8(data: list):
+def validUTF8(data):
     """
-    function to check if data is a valid UTF-8
-    encoding
-    data: list of integers
+    data: a list of integers
     Return: True if data is a valid UTF-8
     encoding, else return False
     """
-    byte_counter = 0
+    byte_count = 0
 
     for i in data:
-        if byte_counter == 0:
+        if byte_count == 0:
             if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_counter = 1
+                byte_count = 1
             elif i >> 4 == 0b1110:
-                byte_counter = 2
+                byte_count = 2
             elif i >> 3 == 0b11110:
-                byte_counter = 3
+                byte_count = 3
             elif i >> 7 == 0b1:
                 return False
         else:
             if i >> 6 != 0b10:
                 return False
-            byte_counter -= 1
-    return byte_counter == 0
+            byte_count -= 1
+    return byte_count == 0
